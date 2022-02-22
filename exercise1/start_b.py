afae2dbee2374126e0a2d0b1ae55009db3b8c537
simzim <38910059+simzim@users.noreply.github.com>
2022-02-22 12:37:14 +0200

    Update start_b

--- exercise1/start_b.py
+++ exercise1/start_b.py
@@ -2,38 +2,49 @@ import gym
 import exercise1.car_maze as cm
 import random as rnd
 import time
+import maze_resolver_b
 
 env = cm.CarMazeEnv()
 
-#####################################
-# You code starts here
-#####################################
+
 
 for i_episode in range(1):
     observation = env.reset()
+
+    # --------------------------------------------
+    # simzim code
+    # --------------------------------------------
+
+    maze = {}           # nebutina, bet vis tiek smagu :)
+    current = '0-0'
+    visited = []
+    path = []
+
     for t in range(200000):
         env.render()
+        time.sleep(0.2)
 
+        # sukuriamas taško kaimynų sąrašas
+        neighbours = maze_resolver_b.get_neighbours(observation, current)
+
+        maze[current] = neighbours              # isidedam i maze dicionary for fun :)
+        path.append(current)                    # isidedam i nueita kelia esama taska
+        visited.append(current)                 # visi aplankyti taskai
+
+        # renkames sekanti zingsni
+        step = maze_resolver_b.find_next(neighbours, visited, path)
+        # zingsniuojam
+        action = maze_resolver_b.do_step(current, step)
+        current = step
+
+        observation, reward, done, info = env.step(action)
+
+        if done:
+            print('SVEIKINAM LAIMĖJOTE :)')
+            print("Episode finished after {} timesteps".format(t+1))
+            print("Reward - " + reward)
+            break
         # --------------------------------------------
-        # example code
+        # end of simzim code
         # --------------------------------------------
-        # #action = env.action_space.sample()
-        # time.sleep(0.1)
-        # avalableActions = [x[0] for x in enumerate(observation) if x[1] == 1]
-        # action = avalableActions[rnd.randint(0, len(avalableActions)-1)]
-        # observation, reward, done, info = env.step(action)
-
-        # if done:
-        #     print("Episode finished after {} timesteps".format(t+1))
-        #     print("Reward - " + reward)
-        #     break
-        # --------------------------------------------
-        # end of example code
-        # --------------------------------------------
-
-#####################################
-# You code ends here
-#####################################
-
-
 env.close()
